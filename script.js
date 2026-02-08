477ff763d5d1ef2d9d83aeaefd40b97d77252899
Gorv <Olaverbestbest@gmail.com>
2026-02-08 03:38:55 +0100

    Just another test.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log(process.env.SUPABASE_DATABASE_URL);
+
 const supabase = createClient(
 	process.env.SUPABASE_DATABASE_URL,
 	process.env.SUPABASE_ANON_KEY
