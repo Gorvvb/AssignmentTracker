7506c868c506817d973b158adb2069dbeb1ae95b
Gorv <Olaverbestbest@gmail.com>
2026-02-08 03:25:13 +0100

    Created a small base.

---
+++ script.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+	process.env.SUPABASE_DATABASE_URL,
+	process.env.SUPABASE_ANON_KEY
+);
+
+console.log(supabase);
