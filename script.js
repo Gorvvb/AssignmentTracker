a3fa85e407358623dee180d2449ea4d5acee7b40
Gorv <Olaverbestbest@gmail.com>
2026-02-08 03:44:34 +0100

    Fixed bug.

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-console.log(process.env.SUPABASE_DATABASE_URL);
-
 const supabase = createClient(
-	process.env.SUPABASE_DATABASE_URL,
-	process.env.SUPABASE_ANON_KEY
+	process.meta.env.SUPABASE_DATABASE_URL,
+	process.meta.env.SUPABASE_ANON_KEY
 );
 
 console.log(supabase);
-console.log("Hello, world!");
